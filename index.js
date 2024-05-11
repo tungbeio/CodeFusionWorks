08d3cf3b84bbbc30c452704f882e8d9866df1f48
tungbeio <cguest2011pun0suy@hotmail.com>
2024-05-11 17:17:14 +0700

    hello

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function flattenArray(arr) {
-  return arr.reduce(
-    (acc, curr) => acc.concat(Array.isArray(curr) ? flattenArray(curr) : curr),
-    [],
-  );
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
 }
